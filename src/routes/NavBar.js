75eabbca15bd03d1e114f395ddaa13caf4e57968
becky <xieb3cky@gmail.com>
2023-02-26 13:44:49 -0500

    added loading context to display loading div when fetching user's current location with quickSearch func

--- src/routes/NavBar.js
+++ src/routes/NavBar.js
@@ -1,16 +1,44 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import UserContext from "../auth/UserContext";
+
 import "./NavBar.css"
 
-function NavBar({ signout, signup }) {
-    const { currUser } = useContext(UserContext);
+function NavBar({ signout, signup, search }) {
+    const { currUser, setLoading } = useContext(UserContext);
+    const navigate = useNavigate();
+
+
+    async function handleQuickSearch(evt) {
+        evt.preventDefault();
+        setLoading(true);
+
+        let lat;
+        let lng;
+
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
+            const data = {
+                lat: lat,
+                lng: lng
+            }
+            let result = await search(data);
+            if (result.success) {
+                setTimeout(() => {
+                    setLoading(false);
+                    navigate("/stations");
+                }, 4000)
+            }
+        })
+    }
+
 
     function loggedInNav() {
         return (
             <ul className="links">
                 <li><a href="/search">Search</a></li>
-                <li><a href="/search">Quick Search</a></li>
+                <li><a href="" onClick={handleQuickSearch}>Quick Search</a></li>
                 <li><a href="/profile">Profile</a></li>
                 <li>
                     <Link to="/" onClick={signout}>
@@ -26,7 +54,7 @@ function NavBar({ signout, signup }) {
         return (
             <ul className="links">
                 <li><a href="/search">Search</a></li>
-                <li><a href="/search">Quick Search</a></li>
+                <li><a href="" onClick={handleQuickSearch}>Quick Search</a></li>
                 <li><a href="/login">Log In</a></li>
                 <li><a href="/signup">Sign Up</a></li>
             </ul>
@@ -38,9 +66,7 @@ function NavBar({ signout, signup }) {
             <div class="header-area">
                 <div class="logo"><b> <a href="/">C - Station</a></b></div>
                 {currUser ? loggedInNav() : loggedOutNav()}
-
             </div>
-
         </>
     );
 };
