de4b29ddea2052a25cd924a70eccd6bd53e39a5f
becky <xieb3cky@gmail.com>
2023-02-26 13:43:12 -0500

    test quickSearch logic here, removed

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 // import { Routes, Route, Navigate } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-
+import UserContext from "../auth/UserContext";
 import Homepage from "../homepage/Homepage";
 import StationList from "../stations/StationList";
 
@@ -21,7 +21,8 @@ import SearchForm from "../search/SearchForm";
  * Visiting a non-existant route Navigates to the homepage.
  */
 
-function MainRoutes({ login, signup, search, stations }) {
+function MainRoutes({ login, signup, search, stations, quickSearch }) {
+
   console.debug(
     "Routes",
     `login=${typeof login}`,
@@ -32,24 +33,18 @@ function MainRoutes({ login, signup, search, stations }) {
   return (
     <>
       <Routes>
-
-        <Route exact path="/" element={<Homepage search={search} stations={stations} />}>
+        <Route exact path="/" element={<Homepage search={search} stations={stations} quickSearch={quickSearch} />}>
         </Route>
-
         <Route exact path="/search" element={<SearchForm search={search} />}>
         </Route>
-
         <Route exact path="/login" element={<LoginForm login={login} />}>
         </Route>
-
         <Route exact path="/signup" element={<SignupForm signup={signup} />}>
         </Route>
-
-        <Route exact path="/stations" element={<StationList stations={stations} />}>
+        <Route exact path="/stations" element={<StationList stations={stations} search={search} />}>
         </Route>
         <Route exact path="/profile" element={<ProfileForm />}>
         </Route>
-
       </Routes>
     </>
   );
